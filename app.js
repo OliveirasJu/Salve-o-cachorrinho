3f2c5cf46ba63b9b4ee62db6918c3ca595d0c8fb
OliveirasJu <julia.osillva04@gmail.com>
2024-10-24 10:14:49 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', function () {
+  // Create a map instance and set the initial view coordinates and zoom level
+  var map = L.map('map').setView-23.53367740372587, -46.85290663444664], 13);
+
+
+  // Add a tile layer to the map from OpenStreetMap
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.53367740372587, -46.85290663444664]).addTo(map);
+  marker.bindPopup("Oii, essa é a minha casinha!").openPopup();
+});
