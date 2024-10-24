1c1cb4e52e11d355b5ed33d92ace7fb3a82028f3
OliveirasJu <julia.osillva04@gmail.com>
2024-10-24 10:49:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,30 @@
 // Wait for the document to be ready
 document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
-  var map = L.map('map').setView([-23.53367740372587, -46.85290663444664], 13);
-
-
+  var map = L.map('map').setView([-23.516357107383552, -46.835143207955774], 7);
+ 
+ 
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-
-
-  // Create a marker with popup and add it to the map
-  var marker = L.marker([-23.53367740372587, -46.85290663444664]).addTo(map);
-  marker.bindPopup("Oii, essa é a minha casinha!").openPopup();
-});
-
-
-
-var greenIcon = L.icon({
-    iconUrl: 'https://static.vecteezy.com/system/resources/thumbnails/016/761/881/small/the-dog-smiles-because-he-is-happy-png.png',
+ 
+  var greenIcon = L.icon({
+    iconUrl: 'https://png.pngtree.com/png-clipart/20230514/original/pngtree-smile-dog-on-white-background-png-image_9160783.png',
  
     iconSize:     [80, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-
-  // Wait for the document to be ready
-document.addEventListener('DOMContentLoaded', function () {
-  // Create a map instance and set the initial view coordinates and zoom level
-  var map = L.map('map').setView([-23.548108835744276, -46.82994974607978], 13);
-
-
-  // Add a tile layer to the map from OpenStreetMap
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
-
-
-  // Create a marker with popup and add it to the map
-  var marker = L.marker([-23.548108835744276, -46.82994974607978]).addTo(map);
-  marker.bindPopup("O cachorrinho está aqui!").openPopup();
 });
-
-var marker2 = L.marker([-23.548167848079338, -46.829939017243134], {icon: greenIcon}).addTo(map);
+ 
+  // Create a marker with popup and add it to the map
+  var marker = L.marker([-23.516357107383552, -46.835143207955774]).addTo(map);
+  marker.bindPopup("Amada turma de DMD, você está aqui, onde há muitas possibilidades de crescimento !").openPopup();
+ 
+ 
+  
+    // Create a marker with popup and add it to the map
+  var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
   marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+ 
+});
